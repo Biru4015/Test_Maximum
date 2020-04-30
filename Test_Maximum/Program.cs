0de12de1a8d0db0abc71e371380b74c6406e78e5
Biru4015 <rambirendra12@gmail.com>
2020-04-30 21:55:21 +0530

    [Birendra]Add added. main method file

---
+++ Test_Maximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Maximum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Maximum Number");
+        }
+    }
+}
