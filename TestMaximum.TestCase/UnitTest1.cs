ff235704ea08f2888776f383d9846989aec42ab9
Biru4015 <rambirendra12@gmail.com>
2020-04-30 22:02:23 +0530

    [Birendra]Add added. program for finding maximum number given second palace

--- TestMaximum.TestCase/UnitTest1.cs
+++ TestMaximum.TestCase/UnitTest1.cs
@@ -9,12 +9,26 @@ namespace Test_Maximum.TestCase
     [TestFixture]
     class TestMaximum
     {
+        /// <summary>
+        /// Test case 1.1
+        /// </summary>
         [Test]
         public void givenMaxNumFirstPosition_whenFindMaxNum_shouldReturnsMaxFirst()
         {
             MaximumNumber maximumNumber = new MaximumNumber();
             int actualMax = maximumNumber.maximumInteger(225, 100, 14);
             Assert.AreEqual(225, actualMax);
+        }
+
+        /// <summary>
+        /// Test case 1.2
+        /// </summary>
+        [Test]
+        public void givenMaxNumSecondPosition_whenFindMaxNum_shouldReturnsMaxSecond()
+        {
+            MaximumNumber maximumNumber = new MaximumNumber();
+            int actualMax = maximumNumber.maximumInteger(225, 375, 157);
+            Assert.AreEqual(375, actualMax);
         }
     }
   
